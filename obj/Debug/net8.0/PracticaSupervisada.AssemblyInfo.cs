4d34db01dd49a7669c8ac8c4f6b78be7832330c1
TizianoMadonna <tizianomadonna334@gmail.com>
2025-04-09 10:04:40 -0300

    Sacar comentarios

--- obj/Debug/net8.0/PracticaSupervisada.AssemblyInfo.cs
+++ obj/Debug/net8.0/PracticaSupervisada.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PracticaSupervisada")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f1bea88c2b8ef2f86fcb90806096259616f35f8e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+40986195f3360d0780441ba0a7baecdfd836d22e")]
 [assembly: System.Reflection.AssemblyProductAttribute("PracticaSupervisada")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PracticaSupervisada")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
